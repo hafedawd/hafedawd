f7fe5393986a5d7125b959af9c4b640e67d6a379
Hafedawd <hafedawd@gmail.com>
2018-11-28 23:30:29 -0800

    This is my fisrt version

---
+++ scripts/js syntaxes.js
@@ -0,0 +1,15 @@
+
+
+document.querySelector('');
+alert();
+console.load;
+document.write("");
+prompt("please enter name:");
+
+var myarry = ["hafed", 66, "hhhh", 89];
+
+myarry.push("gg");
+
+myarry.pop();
+
+var person={name:"hafed", last:"awad"};
